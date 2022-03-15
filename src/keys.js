49499004893b1d4e7eeb585bc8b5aa26a3923b59
Anibal António <djani454@hotmail.com>
2022-03-15 10:51:57 +0100

    fix: :bug:

--- src/keys.js
+++ src/keys.js
@@ -3,56 +3,56 @@ const keys = [
       dataKey: '81',
       kbd: 'Q',
       sound: 'clap',
-      audio: 'src/sounds/clap.wav'
+      audio: './src/sounds/clap.wav'
     },
     {
       dataKey: '87',
       kbd: 'W',
       sound: 'hihat',
-      audio: 'src/sounds/hihat.wav'
+      audio: './src/sounds/hihat.wav'
     },
     {
       dataKey: '69',
       kbd: 'E',
       sound: 'kick',
-      audio: 'src/sounds/kick.wav'
+      audio: './src/sounds/kick.wav'
     },
     {
       dataKey: '65',
       kbd: 'A',
       sound: 'openhat',
-      audio: 'src/sounds/openhat.wav'
+      audio: './src/sounds/openhat.wav'
     },
     {
       dataKey: '83',
       kbd: 'S',
       sound: 'boom',
-      audio: 'src/sounds/boom.wav'
+      audio: './src/sounds/boom.wav'
     },
     {
       dataKey: '68',
       kbd: 'D',
       sound: 'ride',
-      audio: 'src/sounds/ride.wav'
+      audio: './src/sounds/ride.wav'
     },
     {
       dataKey: '90',
       kbd: 'Z',
       sound: 'snare',
-      audio: 'src/sounds/snare.wav'
+      audio: './src/sounds/snare.wav'
     },
     {
       dataKey: '88',
       kbd: 'X',
       sound: 'tom',
-      audio: 'src/sounds/tom.wav'
+      audio: './src/sounds/tom.wav'
     },
     {
       dataKey: '67',
       kbd: 'C',
       sound: 'tink',
-      audio: 'src/sounds/tink.wav'
+      audio: './src/sounds/tink.wav'
     },
   ]
 
-export default keys;
+  export default keys;
