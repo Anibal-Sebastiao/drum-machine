25c85b5e7aab96617fab8fec9e8edfdd618e7cc8
Anibal António <djani454@hotmail.com>
2022-03-15 10:38:08 +0100

    feat: :sparkles: Add Keypad
    Add keypad for playing sound on click ou key down

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,86 @@ import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import keys from './keys'
+
+
 function App() {
+  const [display, setDisplay] = useState('');
+
+  const playAudio = (e) => {
+
+    const kdb = e.keyCode || e; 
+    setDisplay(keys.find(kbd => kbd.dataKey == [kdb]))
+    const audio = document.querySelector(`audio[data-key="${kdb}"]`);
+    const key = document.querySelector(`.key[data-key="${kdb}"]`);
+    if(!audio) return ;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  const handleClick = (e) => {
+
+    playAudio(e.target.dataset.key)
+  }
+
+  window.addEventListener('keydown', function(e){
+    playAudio(e)
+  });
+
+  function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
+
+  const kbd = document.querySelectorAll('.key');
+  kbd.forEach(key => key.addEventListener('transitionend', removeTransition ));
 
   return (
-    <div className="App">
+    <div className="App" id="drum-machine">
+      {
+        <div id='display' className="my-5 text-center">
+          <p className="h1 text-white">{display && display.sound}</p>
+          </div>
+      }
+      <div className="keys">
+        <KeyPad playAudio={handleClick}/>
+      </div>
     </div>
+  )
+}
+
+const KeyPad = ({playAudio}) => {
+  const kbd = keys.map(key => {
+    return <Key dataKey={key.dataKey} key={key.kbd} kbd={key.kbd} sound={key.sound} audio={key.audio} playAudio={playAudio} />
+  } )
+  return (
+    <>
+      {kbd}
+    </>
+  )
+}
+
+const Audio = ({dataKey, audio, kbd}) => {
+  return (
+    <>
+      <audio className="clip" id={kbd} data-key={dataKey} src={audio}></audio>
+    </>
+  )
+}
+
+
+const Key = ({dataKey, kbd, sound, audio, playAudio}) => {
+  return (
+    <>
+    <div data-key={dataKey} className="key">
+      <kbd className="drum-pad" id={sound} data-key={dataKey} onClick={playAudio}>
+        {kbd}
+        <Audio dataKey={dataKey} audio={audio} kbd={kbd} />
+      </kbd>
+      <span className="sound" id={kbd}>{sound}</span>
+    </div>
+    </>
   )
 }
 
---
+++ src/keys.js
@@ -0,0 +1,58 @@
+const keys = [
+    {
+      dataKey: '81',
+      kbd: 'Q',
+      sound: 'clap',
+      audio: 'src/sounds/clap.wav'
+    },
+    {
+      dataKey: '87',
+      kbd: 'W',
+      sound: 'hihat',
+      audio: 'src/sounds/hihat.wav'
+    },
+    {
+      dataKey: '69',
+      kbd: 'E',
+      sound: 'kick',
+      audio: 'src/sounds/kick.wav'
+    },
+    {
+      dataKey: '65',
+      kbd: 'A',
+      sound: 'openhat',
+      audio: 'src/sounds/openhat.wav'
+    },
+    {
+      dataKey: '83',
+      kbd: 'S',
+      sound: 'boom',
+      audio: 'src/sounds/boom.wav'
+    },
+    {
+      dataKey: '68',
+      kbd: 'D',
+      sound: 'ride',
+      audio: 'src/sounds/ride.wav'
+    },
+    {
+      dataKey: '90',
+      kbd: 'Z',
+      sound: 'snare',
+      audio: 'src/sounds/snare.wav'
+    },
+    {
+      dataKey: '88',
+      kbd: 'X',
+      sound: 'tom',
+      audio: 'src/sounds/tom.wav'
+    },
+    {
+      dataKey: '67',
+      kbd: 'C',
+      sound: 'tink',
+      audio: 'src/sounds/tink.wav'
+    },
+  ]
+
+export default keys;
