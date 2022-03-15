fec2299982f0120ff3ef9a326a03465a3706db50
Anibal António <djani454@hotmail.com>
2022-03-15 11:16:46 +0100

    refactor: :recycle: Add URL to sound assets

--- src/keys.js
+++ src/keys.js
@@ -1,58 +1,58 @@
 const keys = [
-    {
-      dataKey: '81',
-      kbd: 'Q',
-      sound: 'clap',
-      audio: './sounds/clap.wav'
-    },
-    {
-      dataKey: '87',
-      kbd: 'W',
-      sound: 'hihat',
-      audio: './sounds/hihat.wav'
-    },
-    {
-      dataKey: '69',
-      kbd: 'E',
-      sound: 'kick',
-      audio: './sounds/kick.wav'
-    },
-    {
-      dataKey: '65',
-      kbd: 'A',
-      sound: 'openhat',
-      audio: './sounds/openhat.wav'
-    },
-    {
-      dataKey: '83',
-      kbd: 'S',
-      sound: 'boom',
-      audio: './sounds/boom.wav'
-    },
-    {
-      dataKey: '68',
-      kbd: 'D',
-      sound: 'ride',
-      audio: './sounds/ride.wav'
-    },
-    {
-      dataKey: '90',
-      kbd: 'Z',
-      sound: 'snare',
-      audio: './sounds/snare.wav'
-    },
-    {
-      dataKey: '88',
-      kbd: 'X',
-      sound: 'tom',
-      audio: './sounds/tom.wav'
-    },
-    {
-      dataKey: '67',
-      kbd: 'C',
-      sound: 'tink',
-      audio: './sounds/tink.wav'
-    },
-  ]
+  {
+    dataKey: 81,
+    kbd: 'Q',
+    sound: 'Heater-1',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    dataKey: 87,
+    kbd: 'W',
+    sound: 'Heater-2',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    dataKey: 69,
+    kbd: 'E',
+    sound: 'Heater-3',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    dataKey: 65,
+    kbd: 'A',
+    sound: 'Heater-4',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    dataKey: 83,
+    kbd: 'S',
+    sound: 'Clap',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    dataKey: 68,
+    kbd: 'D',
+    sound: 'Open-HH',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    dataKey: 90,
+    kbd: 'Z',
+    sound: "Kick-n'-Hat",
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    dataKey: 88,
+    kbd: 'X',
+    sound: 'Kick',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    dataKey: 67,
+    kbd: 'C',
+    sound: 'Closed-HH',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
 
   export default keys;
